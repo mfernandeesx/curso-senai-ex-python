ee28d5a266d836c410b8fe4565a0c3b313944c14
Maycon Fernandes <mayconfernandeesx@gmail.com>
2025-11-28 08:09:05 -0300

    Refactor salary calculation without loop

--- curso-senai-ex-python/python/salario-do-professor.py
+++ curso-senai-ex-python/python/salario-do-professor.py
@@ -1,24 +1,22 @@
 import os 
 os.system("cls")
 
-resposta ="sim"
 
-while( resposta =="sim"):
-    print ("qual é o seu nivel:\n 1- nivel 1 \n 2- nivel 2 \n 3- nivel 3")
-    nivel = input ("qual é o seu nivel: ")
+print ("qual é o seu nivel:\n 1- nivel 1 \n 2- nivel 2 \n 3- nivel 3")
+nivel = input ("qual é o seu nivel: ")
 
-    qtd_aulas =int(input ("qual é sua qtd de aulas por semana: "))
+qtd_aulas =int(input ("qual é sua qtd de aulas por semana: "))
 
-    if nivel == "1":
-        salario = (qtd_aulas *12) *4
-    elif nivel == "2":
-        salario =(qtd_aulas *17)*4
-    elif nivel == "3":
-        salario = (qtd_aulas *25)*4
-    else:
-        print ("o nivel digitado é invalido ")
-    print (f"o seu salario será: {salario}")
+if nivel == "1":
+    salario = (qtd_aulas *12) *4
+elif nivel == "2":
+    salario =(qtd_aulas *17)*4
+elif nivel == "3":
+    salario = (qtd_aulas *25)*4
+else:
+    print ("o nivel digitado é invalido ")
+    
+print (f"o seu salario será: {salario}")
 
 
-    resposta = input ("dejesa executar novamente? sim ou não: ").lower()
 input ("pressione <enter> para finalizar... ")
